c372902a15ec8262669902a39e05d6a06c1ed006
Omar Gaye <omar.gaye@ibm.com>
2020-05-27 23:45:32 -0400

    DB branch

--- src/main/java/com/ibm/cpat/HelloServlet.java
+++ src/main/java/com/ibm/cpat/HelloServlet.java
@@ -1,20 +1,44 @@
 package com.ibm.cpat;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-@WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
+//@WebServlet(name = "HelloServlet", urlPatterns = {"/HelloServlet"}, loadOnStartup = 0)
 public class HelloServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter writer = response.getWriter();
-        writer.println("Hello CP4A Chapter.");
+        writer.println("Hello CP4A Chapter. Count is: " + callDb());
+    }
+
+    protected int callDb () {
+        int count = 0;
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:comp/env");
+            DataSource ds = (DataSource) envContext.lookup("jdbc/helloServletDS");
+            Connection conn = ds.getConnection();
+            Statement statement = conn.createStatement();
+            String sql = "select count(*) from BLUADMIN.supplier";
+            ResultSet rs = statement.executeQuery(sql);
+            while (rs.next()) {
+                count = rs.getInt(1);
+                System.out.println(String.format("************* Count #%d:", rs.getInt(1)));
+            }
+        } catch (Exception catchAll) {
+            catchAll.printStackTrace();
+        }
+        return count;
     }
 }
